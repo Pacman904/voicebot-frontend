2aee8bc73b8fff5d6996d4fcf8a2a3230bc79455
Pacman904 <palettix3@gmail.com>
2025-03-03 10:53:23 +0100

    Update vad-processor.js

--- vad-processor.js
+++ vad-processor.js
@@ -1,15 +1,34 @@
-processor.port.onmessage = (event) => {
-  if (event.data.type === "speech_started") {
-    isSpeaking = true;
-    dc.send(JSON.stringify({
-      type: "input_audio_stream.start",
-      timestamp: performance.now()
-    }));
-  } else if (event.data.type === "speech_stopped") {
-    isSpeaking = false;
-    dc.send(JSON.stringify({
-      type: "input_audio_stream.stop",
-      timestamp: performance.now()
-    }));
+class VADProcessor extends AudioWorkletProcessor {
+  static get parameterDefinitions() {
+    return {
+      threshold: { type: 'number', defaultValue: 0.1 }
+    };
   }
-};
+
+  constructor() {
+    super();
+    this.speaking = false;
+    this.chunkSize = 4096;
+  }
+
+  process(inputs, outputs, parameters) {
+    const input = inputs[0];
+    const buffer = input[0];
+    const volume = Math.sqrt(buffer.reduce((sum, sample) => sum + sample ** 2, 0) / buffer.length);
+
+    if (volume > this.threshold && !this.speaking) {
+      this.speaking = true;
+      this.port.postMessage({
+        type: "speech_started",
+        audio: Array.from(buffer).map(sample => Math.min(Math.max(sample * 32767, -32768), 32767))
+      });
+    } else if (volume < this.threshold * 0.5 && this.speaking) {
+      this.speaking = false;
+      this.port.postMessage({ type: "speech_stopped" });
+    }
+
+    return true;
+  }
+}
+
+registerProcessor('vad-processor', VADProcessor);
