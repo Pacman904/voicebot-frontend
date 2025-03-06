9898185e7f362d55fb869f999deaf05952d428c3
Pacman904 <palettix3@gmail.com>
2025-03-06 10:20:11 +0100

    Update vad-processor.js

--- vad-processor.js
+++ vad-processor.js
@@ -8,9 +8,8 @@ class VADProcessor extends AudioWorkletProcessor {
     this.speaking = false;
     this.buffer = new Float32Array(512);
     this.index = 0;
-    this.threshold = 0.1; // Fallback-Wert
-    
-    // Initialisiere Parameter sicher
+    this.threshold = 0.1;
+
     this.port.onmessage = (e) => {
       if (e.data.threshold) this.threshold = e.data.threshold;
     };
@@ -20,7 +19,6 @@ class VADProcessor extends AudioWorkletProcessor {
     const input = inputs[0]?.[0];
     if (!input) return true;
 
-    // Sicherer Buffer-Zugriff
     for (let i = 0; i < input.length; i++) {
       this.buffer[this.index] = input[i];
       this.index = (this.index + 1) % 512;
@@ -30,11 +28,9 @@ class VADProcessor extends AudioWorkletProcessor {
   }
 
   analyze() {
-    // Parameter mit Fallback
     const threshold = this.parameters?.get('threshold')?.[0] || this.threshold;
-    
     const energy = this.buffer.reduce((sum, val) => sum + val ** 2, 0) / 512;
-    
+
     if (energy > threshold && !this.speaking) {
       this.speaking = true;
       this.port.postMessage({ type: "speech_started" });
